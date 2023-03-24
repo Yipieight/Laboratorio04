a049ed8a41abc2b403bc71f5101f6d303246e745
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-03-24 00:07:46 -0600

    Ejercicio 3 Resuelto

--- Laboratorio04/Ejercicio04.cs
+++ Laboratorio04/Ejercicio04.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Laboratorio04
 {
@@ -10,7 +11,19 @@ namespace Laboratorio04
     {
         public int[] CalcularBilletes(int cantidad, int[] billetes)
         {
-            return new int[] {};
+            int[] suma = new int[billetes.Length];
+            int disponible = 0;
+            int resta = cantidad;
+            string resultado = "";
+            for (int i = 0; i < billetes.Length; i++)
+            {
+                resta = resta - disponible;
+                suma[i] = resta / billetes[i];
+                disponible = suma[i] * billetes[i];
+                resultado = suma[0]+","+ suma[1]+ "," + suma[2]+ "," + suma[3]+ "," + suma[4];
+            }
+
+            return new int[resultado];
         }
     }
 }
